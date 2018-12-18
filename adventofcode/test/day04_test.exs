992ec4da64688bdfaeef40449f2d3a5e3b2a3b52
Hung Nguyen Viet <hvn@familug.org>
2018-12-18 23:19:34 +0700

    Format

--- adventofcode/test/day04_test.exs
+++ adventofcode/test/day04_test.exs
@@ -7,7 +7,8 @@ defmodule Day04Test do
   end
 
   test "parse date" do
-    assert Day04.parse_datetime("[1518-11-01 00:00] Guard #10 begins shift") == elem(DateTime.from_iso8601("1518-11-01 00:00:00+0000"), 1)
+    assert Day04.parse_datetime("[1518-11-01 00:00] Guard #10 begins shift") ==
+             elem(DateTime.from_iso8601("1518-11-01 00:00:00+0000"), 1)
   end
 
   test "get id" do
@@ -20,18 +21,23 @@ defmodule Day04Test do
   end
 
   test "calculate sleep time" do
-    r = "[1518-11-01 00:00] Guard #10 begins shift
+    r =
+      "[1518-11-01 00:00] Guard #10 begins shift
 [1518-11-01 00:05] falls asleep
 [1518-11-01 00:25] wakes up
 [1518-11-01 00:30] falls asleep
-    [1518-11-01 00:55] wakes up" |> String.split("\n", trim: true) |> Day04.calculate_sleep_time({}, Map.new)
-    assert r == %{10 => Map.merge(
-      Map.new(5..24, fn x -> {x, 1} end),
-      Map.new(30..54, fn x -> {x, 1} end)
-    )
-    }
-  end
+    [1518-11-01 00:55] wakes up"
+      |> String.split("\n", trim: true)
+      |> Day04.calculate_sleep_time({}, Map.new())
 
+    assert r == %{
+             10 =>
+               Map.merge(
+                 Map.new(5..24, fn x -> {x, 1} end),
+                 Map.new(30..54, fn x -> {x, 1} end)
+               )
+           }
+  end
 
   test "solve" do
     r = "[1518-11-01 00:00] Guard #10 begins shift
@@ -40,10 +46,9 @@ defmodule Day04Test do
 [1518-11-02 00:05] falls asleep
 [1518-11-02 00:06] wakes up
 [1518-11-01 00:30] falls asleep
-    [1518-11-01 00:55] wakes up" |> String.split("\n", trim: true) |> Day04.solve
+    [1518-11-01 00:55] wakes up" |> String.split("\n", trim: true) |> Day04.solve()
     assert r == 10 * 5
   end
-
 
   test "solve example" do
     r = "[1518-11-01 00:00] Guard #10 begins shift
@@ -62,11 +67,9 @@ defmodule Day04Test do
 [1518-11-04 00:46] wakes up
 [1518-11-05 00:03] Guard #99 begins shift
 [1518-11-05 00:45] falls asleep
-[1518-11-05 00:55] wakes up"
-|> String.split("\n", trim: true) |> Day04.solve
+[1518-11-05 00:55] wakes up" |> String.split("\n", trim: true) |> Day04.solve()
     assert r == 10 * 24
   end
-
 
   test "solve example part 2" do
     r = "[1518-11-01 00:00] Guard #10 begins shift
@@ -85,8 +88,7 @@ defmodule Day04Test do
 [1518-11-04 00:46] wakes up
 [1518-11-05 00:03] Guard #99 begins shift
 [1518-11-05 00:45] falls asleep
-[1518-11-05 00:55] wakes up"
-|> String.split("\n", trim: true) |> Day04.solve2
+[1518-11-05 00:55] wakes up" |> String.split("\n", trim: true) |> Day04.solve2()
     assert r == 99 * 45
   end
 end
