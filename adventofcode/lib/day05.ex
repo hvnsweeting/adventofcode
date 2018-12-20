7491296b6d65b721b25f8d33a311516713a1603b
Hung Nguyen Viet <hvn@familug.org>
2018-12-20 23:34:27 +0700

    Add Day05 part 1

---
+++ adventofcode/lib/day05.ex
@@ -0,0 +1,82 @@
+defmodule Day05 do
+  # https://adventofcode.com/2018/day/5
+  def hello() do
+    :hello
+  end
+
+  def destroy?(unit1, unit2) do
+    diff(unit1, unit2) == 32
+  end
+
+  # O(n) solution
+  # put thing we checked to a stack
+  # if we meet new char, check only top of stack , destroy or go ahead
+  def react_stack(polymer) do
+    do_react_stack(String.to_charlist(polymer), []) |> List.to_string
+  end
+
+  defp do_react_stack([], stack) do
+    stack
+  end
+  defp do_react_stack([h|t], []) do
+    do_react_stack(t, [h])
+  end
+  defp do_react_stack([h|t], stack) do
+    if destroy?(h, List.last(stack)) do
+      do_react_stack(t, List.delete_at(stack, -1))
+    else
+      do_react_stack(t, stack ++ [h])
+    end
+  end
+
+  # Naive implementation, too slow so have to think of react_stack O(n) solution
+  def react(polymer) do
+    once = react_once(polymer)
+    if once == polymer do
+      polymer
+    else
+      react(once)
+    end
+  end
+
+  def react_once(s) do
+    String.to_charlist(s) |> do_react_once(nil, []) |> List.to_string
+  end
+  defp do_react_once([], last_result, result) do
+    if last_result == nil do
+      result
+    else
+      result ++ [last_result]
+    end
+  end
+  defp do_react_once([h|t], nil, result) do
+    do_react_once(t, h, result)
+  end
+  defp do_react_once([h|t], last_unit, result) do
+    if destroy?(h, last_unit) do
+      #result ++ t
+      do_react_once(t, nil, result)
+    else
+      do_react_once(t, h, result ++ [last_unit])
+    end
+  end
+
+  # This react once per iterate is super inefficient
+  #defp do_react_once([h|t], last_unit, result) do
+  #  if destroy?(h, last_unit) do
+  #    result ++ t
+  #  else
+  #    do_react_once(t, h, result ++ [last_unit])
+  #  end
+  #end
+
+
+  def diff(c1, c2) do
+    abs( c1 - c2)
+  end
+
+  def char_diff(c1, c2) do
+    abs(List.first(c1) - List.first(c2))
+  end
+
+end
---
+++ adventofcode/test/day05_test.exs
@@ -0,0 +1,37 @@
+defmodule Day05Test do
+  use ExUnit.Case
+  doctest Day05
+
+  test "greets the world" do
+    assert Day05.hello() == :hello
+  end
+
+  test "diff of c and C is 32" do
+    assert Day05.char_diff('c', 'C') == 32
+    assert Day05.char_diff('C', 'c') == 32
+  end
+
+  ## This test only work with slow version
+  #test "react once dabAcCaCBAcCcaDA  The first 'cC' is removed." do
+  #  assert Day05.react_once("dabAcCaCBAcCcaDA") == "dabAaCBAcCcaDA"
+  #end
+
+
+  test "react once dabCBAcaDA does not change" do
+    assert Day05.react_once("dabCBAcaDA") == "dabCBAcaDA"
+  end
+  test "react stack got correct resutl dabCBAcaDA does not change" do
+    assert Day05.react_stack("dabAcCaCBAcCcaDA") == "dabCBAcaDA"
+  end
+
+
+  test "example part1" do
+    #dabAcCaCBAcCcaDA  The first 'cC' is removed.
+    #dabAaCBAcCcaDA    This creates 'Aa', which is removed.
+    #dabCBAcCcaDA      Either 'cC' or 'Cc' are removed (the result is the same).
+    #dabCBAcaDA        No further actions can be taken.
+    s = "dabAcCaCBAcCcaDA"
+    assert Day05.react(s) == "dabCBAcaDA"
+  end
+
+end
