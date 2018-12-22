e21fb75250fd91226eb01c83feedc6edf30b12ec
Hung Nguyen Viet <hvn@familug.org>
2018-12-22 10:03:14 +0700

    Use MapSet, format

--- adventofcode/lib/day05.ex
+++ adventofcode/lib/day05.ex
@@ -12,16 +12,18 @@ defmodule Day05 do
   # put thing we checked to a stack
   # if we meet new char, check only top of stack , destroy or go ahead
   def react_stack(polymer) do
-    do_react_stack(String.to_charlist(polymer), []) |> List.to_string
+    do_react_stack(String.to_charlist(polymer), []) |> List.to_string()
   end
 
   defp do_react_stack([], stack) do
     stack
   end
-  defp do_react_stack([h|t], []) do
+
+  defp do_react_stack([h | t], []) do
     do_react_stack(t, [h])
   end
-  defp do_react_stack([h|t], stack) do
+
+  defp do_react_stack([h | t], stack) do
     if destroy?(h, List.last(stack)) do
       do_react_stack(t, List.delete_at(stack, -1))
     else
@@ -32,6 +34,7 @@ defmodule Day05 do
   # Naive implementation, too slow so have to think of react_stack O(n) solution
   def react(polymer) do
     once = react_once(polymer)
+
     if once == polymer do
       polymer
     else
@@ -40,8 +43,9 @@ defmodule Day05 do
   end
 
   def react_once(s) do
-    String.to_charlist(s) |> do_react_once(nil, []) |> List.to_string
+    String.to_charlist(s) |> do_react_once(nil, []) |> List.to_string()
   end
+
   defp do_react_once([], last_result, result) do
     if last_result == nil do
       result
@@ -49,12 +53,14 @@ defmodule Day05 do
       result ++ [last_result]
     end
   end
-  defp do_react_once([h|t], nil, result) do
+
+  defp do_react_once([h | t], nil, result) do
     do_react_once(t, h, result)
   end
-  defp do_react_once([h|t], last_unit, result) do
+
+  defp do_react_once([h | t], last_unit, result) do
     if destroy?(h, last_unit) do
-      #result ++ t
+      # result ++ t
       do_react_once(t, nil, result)
     else
       do_react_once(t, h, result ++ [last_unit])
@@ -62,53 +68,47 @@ defmodule Day05 do
   end
 
   # This react once per iterate is super inefficient
-  #defp do_react_once([h|t], last_unit, result) do
+  # defp do_react_once([h|t], last_unit, result) do
   #  if destroy?(h, last_unit) do
   #    result ++ t
   #  else
   #    do_react_once(t, h, result ++ [last_unit])
   #  end
-  #end
-
+  # end
 
   def diff(c1, c2) do
-    abs( c1 - c2)
+    abs(c1 - c2)
   end
 
   def char_diff(c1, c2) do
     abs(List.first(c1) - List.first(c2))
   end
 
-
   # This is not super fast but still under 1 min, may have way to optimize it
   def solve_part2(polymer) do
-    units = all_uniq_units(polymer)
-    units |> String.to_charlist |> Enum.map(fn c -> remove_one_unit_type(c, polymer) end)
-    |> Enum.min_by(&String.length/1) |> String.length
+    polymer
+    |> all_uniq_units
+    |> Enum.map(fn c -> remove_one_unit_type(c, polymer) end)
+    |> Enum.min_by(&String.length/1)
+    |> String.length()
   end
 
   def remove_one_unit_type(unit_type, polymer) do
     unit_order = unit_type
-    other_polarity = if unit_order >= List.first('a') do
-      unit_order - 32
-    else
-      unit_order + 32
-    end
-    String.replace(polymer, List.to_string([unit_order]), "") |> String.replace(List.to_string([other_polarity]), "") |> react_stack
+
+    other_polarity =
+      if unit_order >= List.first('a') do
+        unit_order - 32
+      else
+        unit_order + 32
+      end
+
+    String.replace(polymer, List.to_string([unit_order]), "")
+    |> String.replace(List.to_string([other_polarity]), "")
+    |> react_stack
   end
 
   def all_uniq_units(polymer) do
-    polymer |> String.downcase |> String.to_charlist |> do_all_units([])
-
-  end
-  def do_all_units([], result) do
-    result |> List.to_string
-  end
-  def do_all_units([h|t], result) do
-    if Enum.member?(result, h) do
-      do_all_units(t, result)
-    else
-      do_all_units(t, result ++ [h])
-    end
+    polymer |> String.downcase() |> String.to_charlist() |> MapSet.new() |> MapSet.to_list()
   end
 end
--- adventofcode/test/day05_test.exs
+++ adventofcode/test/day05_test.exs
@@ -12,14 +12,14 @@ defmodule Day05Test do
   end
 
   ## This test only work with slow version
-  #test "react once dabAcCaCBAcCcaDA  The first 'cC' is removed." do
+  # test "react once dabAcCaCBAcCcaDA  The first 'cC' is removed." do
   #  assert Day05.react_once("dabAcCaCBAcCcaDA") == "dabAaCBAcCcaDA"
-  #end
-
+  # end
 
   test "react once dabCBAcaDA does not change" do
     assert Day05.react_once("dabCBAcaDA") == "dabCBAcaDA"
   end
+
   test "react stack got correct resutl dabCBAcaDA does not change" do
     assert Day05.react_stack("dabAcCaCBAcCcaDA") == "dabCBAcaDA"
   end
@@ -28,23 +28,20 @@ defmodule Day05Test do
     assert Day05.solve_part2("dabAcCaCBAcCcaDA") == 4
   end
 
-
   test "all uniq units in abaBacC is abc" do
-    assert Day05.all_uniq_units("abaBacC") == "abc"
+    assert Day05.all_uniq_units("abaBacC") == 'abc'
   end
 
   test "remove a unit" do
     assert Day05.remove_one_unit_type(List.first('a'), "dabAcCaCBAcCcaDA") == "dbCBcD"
   end
 
-
   test "example part1" do
-    #dabAcCaCBAcCcaDA  The first 'cC' is removed.
-    #dabAaCBAcCcaDA    This creates 'Aa', which is removed.
-    #dabCBAcCcaDA      Either 'cC' or 'Cc' are removed (the result is the same).
-    #dabCBAcaDA        No further actions can be taken.
+    # dabAcCaCBAcCcaDA  The first 'cC' is removed.
+    # dabAaCBAcCcaDA    This creates 'Aa', which is removed.
+    # dabCBAcCcaDA      Either 'cC' or 'Cc' are removed (the result is the same).
+    # dabCBAcaDA        No further actions can be taken.
     s = "dabAcCaCBAcCcaDA"
     assert Day05.react(s) == "dabCBAcaDA"
   end
-
 end
