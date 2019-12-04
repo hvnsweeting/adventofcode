eef39298d87d812ebc03054c6180db91add25175
Viet Hung Nguyen <hvn@familug.org>
2019-12-04 16:08:21 +0700

    Add day 4 part 2

--- adventofcode/lib/aoc2019_day4.ex
+++ adventofcode/lib/aoc2019_day4.ex
@@ -23,11 +23,11 @@ defmodule Aoc2019Day4 do
     end
   end
 
-  def has_two_adjacent_digits?([]) do
+  def has_at_least_to_same_adjacent_digits?([]) do
     false
   end
 
-  def has_two_adjacent_digits?([h | t]) do
+  def has_at_least_to_same_adjacent_digits?([h | t]) do
     next = List.first(t)
 
     if next == nil do
@@ -36,8 +36,26 @@ defmodule Aoc2019Day4 do
       if h == next do
         true
       else
-        has_two_adjacent_digits?(t)
+        has_at_least_to_same_adjacent_digits?(t)
       end
+    end
+  end
+
+  def has_two_adjacent_digits?([h | t]) do
+    has_two_adjacent_digits?(t, h, 1, [])
+  end
+
+  defp has_two_adjacent_digits?([], tmp, count, acc) do
+    final = acc ++ [{tmp, count}]
+    good = final |> Enum.filter(fn {x, count} -> count == 2 end)
+    length(good) > 0
+  end
+
+  defp has_two_adjacent_digits?([h | t], tmp, count, acc) do
+    if h == tmp do
+      has_two_adjacent_digits?(t, h, count + 1, acc)
+    else
+      has_two_adjacent_digits?(t, h, 1, acc ++ [{tmp, count}])
     end
   end
 
@@ -66,6 +84,17 @@ defmodule Aoc2019Day4 do
     Enum.all?([
       has_six_digits_number?(pw_charlist),
       is_within_range?(password, start, stop),
+      has_at_least_to_same_adjacent_digits?(String.to_charlist(password)),
+      is_never_decrease?(String.to_charlist(password))
+    ])
+  end
+
+  def valid_password2?(password, start, stop) do
+    pw_charlist = String.to_charlist(password)
+
+    Enum.all?([
+      has_six_digits_number?(pw_charlist),
+      is_within_range?(password, start, stop),
       has_two_adjacent_digits?(String.to_charlist(password)),
       is_never_decrease?(String.to_charlist(password))
     ])
@@ -76,6 +105,18 @@ defmodule Aoc2019Day4 do
     |> Enum.map(&Integer.to_string/1)
     |> Enum.reduce(0, fn x, acc ->
       if valid_password?(x, start, stop) do
+        acc + 1
+      else
+        acc
+      end
+    end)
+  end
+
+  def solve_part_2(start, stop) do
+    start..stop
+    |> Enum.map(&Integer.to_string/1)
+    |> Enum.reduce(0, fn x, acc ->
+      if valid_password2?(x, start, stop) do
         acc + 1
       else
         acc
--- adventofcode/test/aco2019_day04_test.exs
+++ adventofcode/test/aco2019_day04_test.exs
@@ -7,12 +7,16 @@ defmodule Aco2019Day04Test do
     assert Aoc2019Day4.solve_part_1(@start, @stop) == 466
   end
 
+  test "Solve part 2" do
+    assert Aoc2019Day4.solve_part_2(@start, @stop) == 292
+  end
+
   test "111111 meets these criteria (double 11, never decreases)." do
     assert Aoc2019Day4.valid_password?("111111", 100_000, 200_000) == true
   end
 
   test "388888 meets these criteria (double 88, never decreases)." do
-    assert Aoc2019Day4.has_two_adjacent_digits?(String.to_charlist("388888")) == true
+    assert Aoc2019Day4.has_at_least_to_same_adjacent_digits?(String.to_charlist("388888")) == true
     assert Aoc2019Day4.valid_password?("388888", 100_000, 500_000) == true
   end
 
@@ -22,5 +26,17 @@ defmodule Aco2019Day04Test do
 
   test " 123789 does not meet these criteria (no double). " do
     assert Aoc2019Day4.valid_password?("123789", 100_000, 200_000) == false
+  end
+
+  test "  112233 meets these criteria because the digits never decrease and all repeated digits are exactly two digits long." do
+    assert Aoc2019Day4.has_two_adjacent_digits?(String.to_charlist("112233")) == true
+  end
+
+  test "  123444 no longer meets the criteria (the repeated 44 is part of a larger group of 444)." do
+    assert Aoc2019Day4.has_two_adjacent_digits?(String.to_charlist("123444")) == false
+  end
+
+  test "  111122 meets the criteria (even though 1 is repeated more than twice, it still contains a double 22)." do
+    assert Aoc2019Day4.has_two_adjacent_digits?(String.to_charlist("111122")) == true
   end
 end
