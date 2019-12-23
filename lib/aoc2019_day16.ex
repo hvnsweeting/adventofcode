6cda26587e27664b444e77e32e1680607ee50435
Viet Hung Nguyen <hvn@familug.org>
2019-12-23 09:30:23 +0700

    Done day 16

--- lib/aoc2019_day16.ex
+++ lib/aoc2019_day16.ex
@@ -1,29 +1,27 @@
 defmodule Aoc2019Day16 do
   # https://adventofcode.com/2019/day/16
   def as_list(text) do
-    text |> String.trim() |> String.to_charlist() |> Enum.map(fn d -> d - ?0 end)
-  end
-
-  def apply_phases(text, c, drop \\ 1)
-
-  def apply_phases(text, 0, drop) do
     text
-    |> Enum.map(&Integer.to_string/1)
-    |> Enum.join()
+    |> String.trim()
+    |> String.to_charlist()
+    |> Enum.map(fn d -> d - ?0 end)
+    |> Enum.with_index(1)
   end
 
-  def apply_phases(text, phases, drop) do
-    apply_phases(apply_phase(text, drop), phases - 1, drop)
+  @doc """
+  This originally used for part 1 but now keep for reference only after refactor
+  """
+  def raw_pattern(base_pattern, nth) do
+    Stream.flat_map(base_pattern, fn x -> Stream.cycle([x]) |> Enum.take(nth) end)
+    |> Stream.cycle()
   end
 
-  def apply_phase(input, drop \\ 1) do
-    # IO.inspect({"Input length", length(input), "drop", drop, drop..length(input)})
 
-    drop..length(input)
-    |> Enum.map(fn nth ->
-      real_pattern = Stream.drop(raw_pattern([0, 1, 0, -1], nth), 1)
-      # IO.inspect(Enum.take(real_pattern, 20))
-      make_element(input, real_pattern) |> ones_digit
+  def doone(numbers) do
+    numbers_with_index = Enum.with_index(numbers)
+
+    Enum.map(numbers_with_index, fn {{_, nth}, local_index} ->
+      calculate(numbers, nth, local_index)
     end)
   end
 
@@ -31,19 +29,82 @@ defmodule Aoc2019Day16 do
     rem(abs(number), 10)
   end
 
-  def make_element(input_list, pattern) do
-    Stream.zip(input_list, Stream.cycle(pattern))
-    |> Stream.map(fn {e, p} -> e * p end)
-    |> Enum.sum()
-
-    # |> Enum.drop(skip)
-    # IO.inspect({"Remain", length(v)})
-    # v |> Enum.sum()
+  def apply_phases(text, 0) do
+    text
+    |> Enum.map(fn {n, _} -> n end)
+    |> Enum.map(&Integer.to_string/1)
+    |> Enum.join()
   end
 
-  def raw_pattern(base_pattern, nth) do
-    Stream.flat_map(base_pattern, fn x -> Stream.cycle([x]) |> Enum.take(nth) end)
-    |> Stream.cycle()
+  def apply_phases(text, phases) do
+    output = doone(text)
+    apply_phases(output, phases - 1)
+  end
+
+  @doc """
+  Instead of calculate 0 1 0 -1 stream and zip with numbers,
+  this calculates result base on pattern of 0 1 0 -1
+  """
+  def calculate(numbers, nth, drop \\ 0) do
+    dropped = numbers |> Enum.drop(drop)
+
+    r =
+      dropped
+      |> Enum.reduce(0, fn x, acc ->
+        {n, idx} = x
+        idx = idx + 1
+        remainder = rem(idx, nth * 4)
+
+        v =
+          case ceil(remainder / nth) do
+            4 -> -n
+            1 -> 0
+            2 -> n
+            3 -> 0
+            0 -> -n
+          end
+
+        acc + v
+      end)
+      # |> IO.inspect #|> ones_digit
+      |> ones_digit
+
+    {r, nth}
+  end
+
+  @doc """
+  Part2 caculate in a totally different way. The input is huge, using
+  method in part1 would never work (too slow).
+  This method based on lot of observations:
+  - row nth, there would be n 1 numbers in pattern
+  - row nth, result would depends on col nth forward, because before it is all multiply with 0
+  - base on part 2 examples and input, the offset is big enough to guarantee
+  all numbers would still part of multiply by 1. E.g offset 5_000_000 on 500_000
+  remaining number (ie total lengh 5_500_000)
+  - as we care only about 1 last digit, there is no need to calculate huge sum,
+  just need to ensure the part ensure last digit -> add 10 to it to avoid
+  negative
+  """
+  def generate_new_number(numbers) do
+    sum_base = ones_digit(Enum.sum(numbers))
+
+    numbers
+    |> Enum.reduce({[], sum_base, 0}, fn n, {ls, cumsum, last} ->
+      {[ones_digit(10 + cumsum - last) | ls], 10 + cumsum - last, n}
+    end)
+    |> elem(0)
+    |> Enum.reverse()
+  end
+
+  def loop(text, 0) do
+    text
+    |> Enum.map(&Integer.to_string/1)
+    |> Enum.join()
+  end
+
+  def loop(text, phases) do
+    output = generate_new_number(text)
+    loop(output, phases - 1)
   end
 
   def solve1(text, phases \\ 100) do
@@ -51,15 +112,18 @@ defmodule Aoc2019Day16 do
     apply_phases(input, phases) |> String.slice(0, 8)
   end
 
-  def solve2(text, phases \\ 100) do
-    offset = String.to_integer(String.slice(text, 0, 7))
-    IO.inspect({"OFFSET", offset})
+  def solve2(input, phases \\ 100) do
+    offset = String.to_integer(String.slice(input, 0, 7))
+    inp = input |> String.trim()
 
-    [text]
-    |> Stream.cycle()
-    |> Enum.take(10000)
-    |> Enum.join()
-    |> as_list
-    |> apply_phases(100, offset)
+    inp =
+      inp
+      |> String.to_charlist()
+      |> Enum.map(fn d -> d - ?0 end)
+      |> Stream.cycle()
+      |> Enum.take(10000 * String.length(inp))
+      |> Enum.drop(offset)
+
+    Aoc2019Day16.loop(inp, phases) |> String.slice(0, 8)
   end
 end
--- test/aoc2019_day16_test.exs
+++ test/aoc2019_day16_test.exs
@@ -1,6 +1,7 @@
 defmodule Aoc2019Day16Test do
   use ExUnit.Case
 
+  @input "59734319985939030811765904366903137260910165905695158121249344919210773577393954674010919824826738360814888134986551286413123711859735220485817087501645023012862056770562086941211936950697030938202612254550462022980226861233574193029160694064215374466136221530381567459741646888344484734266467332251047728070024125520587386498883584434047046536404479146202115798487093358109344892308178339525320609279967726482426508894019310795012241745215724094733535028040247643657351828004785071021308564438115967543080568369816648970492598237916926533604385924158979160977915469240727071971448914826471542444436509363281495503481363933620112863817909354757361550"
   test "Then, only the ones digit is kept: 38 becomes 8" do
     assert Aoc2019Day16.ones_digit(38) == 8
   end
@@ -9,11 +10,11 @@ defmodule Aoc2019Day16Test do
     assert Aoc2019Day16.ones_digit(-17) == 7
   end
 
-  test "if the input list were 9, 8, 7, 6, 5 and the pattern for a given element were 1, 2, 3, the result would be 9*1 + 8*2 + 7*3 + 6*1 + 5*2" do
-    input_list = [9, 8, 7, 6, 5]
-    pattern = [1, 2, 3]
-    assert Aoc2019Day16.make_element(input_list, pattern) == 9 * 1 + 8 * 2 + 7 * 3 + 6 * 1 + 5 * 2
-  end
+  # test "if the input list were 9, 8, 7, 6, 5 and the pattern for a given element were 1, 2, 3, the result would be 9*1 + 8*2 + 7*3 + 6*1 + 5*2" do
+  #  input_list = [9, 8, 7, 6, 5]
+  #  pattern = [1, 2, 3]
+  #  assert Aoc2019Day16.make_element(input_list, pattern) == 9 * 1 + 8 * 2 + 7 * 3 + 6 * 1 + 5 * 2
+  # end
 
   test "Raw pattern for 3rd elem is 0, 0, 0, 1, 1, 1, 0, 0, 0, -1, -1, -1 from base pattern 0, 1, 0, -1. " do
     nth = 3
@@ -47,11 +48,11 @@ defmodule Aoc2019Day16Test do
     assert Aoc2019Day16.solve1("12345678", 2) == "34040438"
   end
 
-  test "Input signal 12345678 -> FFT After 3 phase: 34040438" do
+  test "Input signal 12345678 -> FFT After 3 phase: 03415518" do
     assert Aoc2019Day16.solve1("12345678", 3) == "03415518"
   end
 
-  test "Input signal 12345678 -> FFT After 4 phase: 34040438" do
+  test "Input signal 12345678 -> FFT After 4 phase: 01029498" do
     assert Aoc2019Day16.solve1("12345678", 4) == "01029498"
   end
 
@@ -60,13 +61,104 @@ defmodule Aoc2019Day16Test do
   end
 
   @tag slow: true
-  test "solve 1" do
-    {:ok, input} = File.read("test/input2019_16_1.txt")
-    assert Aoc2019Day16.solve1(input) == "37153056"
+  # test "solve 1" do
+  #  {:ok, input} = File.read("test/input2019_16_1.txt")
+  #  assert Aoc2019Day16.solve1(input) == "37153056"
+  # end
+
+  test "Input signal 12345678 -> FFT After 1 phase: 48226158 offset 3 -> 26158" do
+    inp = Aoc2019Day16.as_list("12345678") |> Enum.drop(3)
+    assert Aoc2019Day16.apply_phases(inp, 1) == "26158"
   end
 
-  # test "03036732577212944063491565474664 x 10000 times -> FFT 100 phases  becomes 84462026 " do
-  #   input = "03036732577212944063491565474664"
-  #   assert Aoc2019Day16.solve2(input) == "84462026"
-  # end
+  test "signal 12345678 -> FFT After 4 phases: offset 3 -> 010_29498 -> 29498" do
+    inp = Aoc2019Day16.as_list("12345678") |> Enum.drop(3)
+    assert Aoc2019Day16.apply_phases(inp, 4) == "29498"
+  end
+
+  test "signal 69317163492948606335995924319873  -> FFT After 100 phases:52432133 " do
+    inp = Aoc2019Day16.as_list("69317163492948606335995924319873")
+    assert Aoc2019Day16.apply_phases(inp, 100) |> String.slice(0, 8) == "52432133"
+  end
+
+  test "new way to calculate 1 digit without create 0 1 0 -1 stream" do
+    # input = "12345678"
+    inp = Aoc2019Day16.as_list("12345678")
+    assert Aoc2019Day16.calculate(inp, 1) == {4, 1}
+    assert Aoc2019Day16.calculate(inp, 2) == {8, 2}
+
+    assert Aoc2019Day16.doone(inp)
+           |> Enum.map(fn {v, _} -> Integer.to_string(v) end)
+           |> Enum.join("") == "48226158"
+
+    assert Aoc2019Day16.doone(inp)
+           |> Aoc2019Day16.doone()
+           |> Enum.map(fn {v, _} -> Integer.to_string(v) end)
+           |> Enum.join("") == "34040438"
+
+    assert Aoc2019Day16.doone(inp)
+           |> Enum.drop(3)
+           |> Enum.map(fn {v, _} -> Integer.to_string(v) end)
+           |> Enum.join("") == "26158"
+
+    assert Aoc2019Day16.doone(inp)
+           |> Aoc2019Day16.doone()
+           |> Enum.map(fn {v, _} -> Integer.to_string(v) end)
+           |> Enum.drop(3)
+           |> Enum.join("") == "40438"
+
+    inp = Aoc2019Day16.as_list("12345123451234512345")
+
+    assert Aoc2019Day16.doone(inp)
+           |> Enum.map(fn {v, _} -> Integer.to_string(v) end)
+           |> Enum.join("") == "06030407700974054295"
+  end
+
+  @tag timeout: :infinity
+  test "03036732577212944063491565474664 x 10000 times -> FFT 1 phases  " do
+    # FFT 100 times becomes 84462026 but slow
+    input = "03036732577212944063491565474664"
+
+    inp =
+      input
+      |> String.to_charlist()
+      |> Enum.map(fn d -> d - ?0 end)
+      |> Stream.cycle()
+      |> Stream.with_index(1)
+      |> Enum.take(10000 * String.length(input))
+      |> Enum.drop(303_673)
+
+    assert Aoc2019Day16.apply_phases(inp, 1) |> String.slice(0, 8) == "61706040"
+  end
+
+  @tag timeout: :infinity
+  test "FAST 12345 x 4 times, offset 14 becomes 054295 " do
+    input = "12345"
+
+    inp =
+      input
+      |> String.to_charlist()
+      |> Enum.map(fn d -> d - ?0 end)
+      |> Stream.cycle()
+      |> Enum.take(20)
+
+    assert Aoc2019Day16.loop(inp, 1) |> String.slice(-6, 6) == "054295"
+  end
+
+  @tag timeout: :infinity
+  test "FAST 03036732577212944063491565474664 x 10000 times -> FFT 100 phases  becomes 84462026 " do
+    input = "03036732577212944063491565474664"
+    assert Aoc2019Day16.solve2(input, 100) == "84462026"
+    input = "02935109699940807407585447034323"
+    assert Aoc2019Day16.solve2(input, 100) == "78725270"
+    input = "03081770884921959731165446850517"
+    assert Aoc2019Day16.solve2(input, 100) == "53553731"
+    assert Aoc2019Day16.solve2(@input, 100) == "60592199"
+  end
+
+  test "solve 2" do
+    {:ok, input} = File.read("test/input2019_16_1.txt")
+    assert input |> String.trim() == @input
+    assert Aoc2019Day16.solve2(input, 100) == "60592199"
+  end
 end
