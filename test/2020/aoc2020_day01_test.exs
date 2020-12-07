85fa0dbdcfdf7d8eda57727eb0e0cd78af3f05c4
Viet Hung Nguyen <hvn@familug.org>
2020-12-07 18:30:46 +0700

    Move

--- test/2020/aoc2020_day01_test.exs
+++ test/2020/aoc2020_day01_test.exs
@@ -2,7 +2,7 @@ defmodule Aoc2020Day01Test do
   use ExUnit.Case
 
   test "solve1" do
-    {:ok, input} = File.read("test/aoc2020_day01.txt")
+    {:ok, input} = File.read("test/input2020_1")
     #    #input = "1721
     # 979
     # 366
@@ -13,7 +13,7 @@ defmodule Aoc2020Day01Test do
   end
 
   test "solve2" do
-    {:ok, input} = File.read("test/aoc2020_day01.txt")
+    {:ok, input} = File.read("test/input2020_1")
     assert Aoc2020Day01.solve2(input) == 165_026_160
   end
 end
