e475abd24c1b26255186e862ba492435ca7fe76b
Viet Hung Nguyen <hvn@familug.org>
2020-12-07 14:03:52 +0700

    Add day 7

--- lib/aoc2020_day07.ex
+++ lib/aoc2020_day07.ex
@@ -2,52 +2,78 @@ defmodule Aoc2020Day07 do
   import Enum
 
   def solve1(input) do
-    rules = input
-            |> String.replace(" bag.", " bags.")
-            |> String.replace(" bag,", " bags,")
-    |> String.trim()
-    |> String.split("\n")
-    |> map(&(String.trim(&1, ".")))
-    |> map(fn x -> x |> String.split("contain") end)
-    |> map(fn [h|t] ->
-      IO.inspect(h)
-      {h|>String.trim, parse_inside(t)}
-    end)
+    rm = make_map(input)
+    ks = rm |> Map.keys()
 
-
-
-    rm = Map.new(rules)
-
-    ks = rm |> Map.keys
-    ks |> Enum.filter(fn k -> contain(rm, k, "shiny gold bag") end)
-    |> IO.inspect
+    ks
+    |> Enum.filter(fn k -> contain(rm, k, "shiny gold bag") end)
+    |> IO.inspect()
     |> length
-
-
   end
 
-  def parse_inside([h|_]) do
+  def make_map(input) do
+    rules =
+      input
+      |> String.replace(" bag.", " bags.")
+      |> String.replace(" bag,", " bags,")
+      |> String.trim()
+      |> String.split("\n")
+      |> map(&String.trim(&1, "."))
+      |> map(fn x -> x |> String.split("contain") end)
+      |> map(fn [h | t] ->
+        IO.inspect(h)
+        {h |> String.trim(), parse_inside(t)}
+      end)
+      |> Map.new()
+  end
+
+  def solve2(input) do
+    rm = make_map(input)
+    mcount(rm, "shiny gold bags") - 1
+  end
+
+  def mcount(ms, key) do
+    if Map.get(ms, key) == [] do
+      1
+    else
+      v = Map.get(ms, key)
+
+      if v == nil do
+        IO.inspect({key, nil})
+      end
+
+      r =
+        v
+        |> map(fn {n, k} ->
+          n * mcount(ms, k)
+        end)
+        |> sum
+
+      r + 1
+    end
+  end
+
+  def parse_inside([h | _]) do
     h
-    |> String.trim
+    |> String.trim()
     |> String.split(",", trim: true)
-    |> map(fn x -> x |> String.trim end)
+    |> map(fn x -> x |> String.trim() end)
     |> filter(fn x -> x != "no other bags" end)
     |> map(fn x ->
-      [h,t|r] = String.split(x, " ", parts: 2)
-      t
-    end
-      )
-
-
+      [h, t | r] = String.split(x, " ", parts: 2)
+      {String.to_integer(h), t}
+    end)
   end
 
   def contain(rules, key, bag) do
     ks = Map.get(rules, key)
+
     if ks == nil do
       IO.inspect({"notfound", key})
       false
     else
-      Enum.any?(ks, fn s -> String.contains?(s, bag) end) || Enum.any?(ks, fn k -> contain(rules, k, bag) end)
+      Enum.any?(ks, fn {_c, s} -> String.contains?(s, bag) end) ||
+        Enum.any?(ks, fn {_c, k} -> contain(rules, k, bag) end)
     end
   end
 end
---
+++ test/aoc2020_day07_test.exs
@@ -0,0 +1,23 @@
+defmodule Aoc2020Day07Test do
+  use ExUnit.Case
+
+  test "p1" do
+    input = "light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.
+    "
+    {:ok, input} = File.read("test/input2020_7")
+    assert Aoc2020Day07.solve1(input) == 242
+  end
+
+  test "p2" do
+    {:ok, input} = File.read("test/input2020_6")
+    assert Aoc2020Day07.solve2(input) == 176_035
+  end
+end
