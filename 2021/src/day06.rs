a9be730efd717211dfabf1edfd03e32bccc6f4c9
Viet Hung Nguyen <hvn@familug.org>
2021-12-05 21:25:03 +0700

    Improve template

--- 2021/src/day06.rs
+++ 2021/src/day06.rs
@@ -1,10 +1,17 @@
+use regex::Regex;
 use std::collections::HashMap;
 use std::collections::HashSet;
 
+fn line_mapper(line: &str) -> u32 {
+    line.parse().unwrap()
+}
+
 pub fn part1(xs: Vec<&str>) -> u32 {
+    println!("xs[0..3] {:?}", &xs[0..3]);
     1
 }
 pub fn part2(xs: Vec<&str>) -> u32 {
+    println!("xs[0..3] {:?}", &xs[0..3]);
     1
 }
 #[cfg(test)]
@@ -16,7 +23,7 @@ mod tests {
     fn test_61() {
         //let s = fs::read_to_string("src/input04").expect("cannot read file");
         let s = "";
-        let xs = s.trim().split("\n\n").collect::<Vec<&str>>();
+        let xs = s.trim().split("\n").collect::<Vec<&str>>();
         let r = part1(xs);
         assert_eq!(r, 0);
     }
@@ -24,8 +31,8 @@ mod tests {
     fn test_62() {
         //let s = fs::read_to_string("src/input04").expect("Cannot read file");
         let s = "";
-        let xs = s.trim().split("\n\n").collect::<Vec<&str>>();
+        let xs = s.trim().split("\n").collect::<Vec<&str>>();
         let r = part2(xs);
-        assert_eq!(r, 1);
+        assert_eq!(r, 0);
     }
 }
