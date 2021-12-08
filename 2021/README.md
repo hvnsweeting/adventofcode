b9b2f93f9cfbbe9003694184a94e7ae8a701dba4
Viet Hung Nguyen <hvn@familug.org>
2021-12-08 18:54:39 +0700

    Refactor

--- 2021/README.md
+++ 2021/README.md
@@ -55,3 +55,6 @@
 - D7. `u64` makes it hard to calculate abs (because there is no negative number), use `i64` as default.
 - D7. `min` and `max` need `.unwrap().cloned()`, because `min` of empty iterator returns `None`.
 - D7. `let &min` to auto deference, avoid to write `.cloned()`.
+- D8. A built-in method can accept `x` or `&x`, it's up to it to own or borrow, and most want borrow only, thus use `&x`.
+  `vec![1,2,3].contains(&2);`
+
--- 2021/src/day08.rs
+++ 2021/src/day08.rs
@@ -14,22 +14,6 @@ pub fn part1(xs: Vec<&str>) -> i64 {
     let mapped: Vec<_> = xs.iter().map(|&line| line_mapper(line)).collect();
     println!("mapped[0..3]: {:?}", &mapped[0..3]);
 
-    // 0: 6
-    // 1
-    let mut d: HashMap<i32, i32> = HashMap::new();
-    d.insert(0, 6);
-    d.insert(1, 2);
-    d.insert(2, 5);
-    d.insert(3, 5);
-    d.insert(4, 4);
-    d.insert(5, 5);
-    d.insert(6, 6);
-    d.insert(7, 3);
-    d.insert(8, 7);
-    d.insert(9, 6);
-
-    dbg!(&d);
-
     let mapped: Vec<_> = mapped
         .iter()
         .map(|&(_, s2)| s2.trim().split(" ").collect::<Vec<&str>>())
@@ -47,19 +31,112 @@ pub fn part1(xs: Vec<&str>) -> i64 {
     }
     counter
 }
+
+fn sum_a_line(signal: (Vec<&str>, Vec<&str>)) -> i64 {
+    let mut one: HashSet<char> = HashSet::new();
+    let mut three: HashSet<char> = HashSet::new();
+    let mut four: HashSet<char> = HashSet::new();
+    let mut five: HashSet<char> = HashSet::new();
+    let mut seven: HashSet<char> = HashSet::new();
+    let mut eight: HashSet<char> = HashSet::new();
+    let (ss, output) = signal;
+    for s in &ss {
+        let h: HashSet<char> = s.clone().chars().collect();
+        if h.len() == 2 {
+            one = h.clone();
+            continue;
+        } else if h.len() == 3 {
+            seven = h.clone();
+            continue;
+        } else if h.len() == 4 {
+            four = h.clone();
+            continue;
+        } else if h.len() == 7 {
+            eight = h.clone();
+            continue;
+        }
+    }
+    for s in &ss {
+        let h: HashSet<char> = s.clone().chars().collect();
+        let mut subcounter = 0;
+        for s2 in &ss {
+            let h2: HashSet<char> = s2.clone().chars().collect();
+            if h.len() == 5 && h != h2 && h.is_subset(&h2) {
+                subcounter += 1;
+            }
+            if h.len() == 5 && one.is_subset(&h) {
+                three = h.clone();
+            }
+        }
+        if subcounter == 3 {
+            five = h;
+        }
+    }
+
+    let mut charmap: Vec<char> = vec![' '; 7];
+    charmap[0] = seven.difference(&one).cloned().collect::<Vec<char>>()[0];
+    charmap[1] = four.difference(&three).cloned().collect::<Vec<char>>()[0];
+    charmap[2] = four.difference(&five).cloned().collect::<Vec<char>>()[0];
+    charmap[3] = four
+        .intersection(&three)
+        .cloned()
+        .collect::<HashSet<char>>()
+        .difference(&one)
+        .cloned()
+        .collect::<Vec<char>>()[0];
+    charmap[4] = eight
+        .difference(&five)
+        .cloned()
+        .collect::<HashSet<char>>()
+        .difference(&one)
+        .cloned()
+        .collect::<Vec<char>>()[0];
+    charmap[5] = five.intersection(&one).cloned().collect::<Vec<char>>()[0];
+    charmap[6] = five
+        .difference(&four)
+        .cloned()
+        .collect::<HashSet<char>>()
+        .difference(&(seven.difference(&one).cloned().collect::<HashSet<char>>()))
+        .cloned()
+        .collect::<Vec<char>>()[0];
+
+    let s = charmap.iter().collect::<String>();
+    let order: &str = s.as_str().clone();
+    let mut d: HashMap<Vec<i64>, i64> = HashMap::new();
+    d.insert(vec![2, 5], 1);
+    d.insert(vec![1, 2, 3, 5], 4);
+    d.insert(vec![0, 2, 5], 7);
+    d.insert(vec![0, 1, 2, 3, 4, 5, 6], 8);
+    d.insert(vec![0, 2, 3, 4, 6], 2);
+    d.insert(vec![0, 2, 3, 5, 6], 3);
+    d.insert(vec![0, 1, 3, 5, 6], 5);
+    d.insert(vec![0, 1, 3, 4, 5, 6], 6);
+    d.insert(vec![0, 1, 2, 4, 5, 6], 0);
+    d.insert(vec![0, 1, 2, 3, 5, 6], 9);
+
+    fn char_to_num(charmap: &str, s: &str, map: &HashMap<Vec<i64>, i64>) -> i64 {
+        let p: HashSet<i64> = s.chars().map(|c| charmap.find(c).unwrap() as i64).collect();
+        for (k, v) in map {
+            let khs: HashSet<i64> = k.iter().cloned().collect();
+            if khs == p {
+                return *v;
+            }
+        }
+        dbg!(&charmap, s);
+
+        panic!("NOT REACH HERR");
+    }
+
+    output
+        .iter()
+        .map(|&s| char_to_num(&order, s, &d))
+        .zip((0..(output.len() as u32)).rev())
+        .map(|(n, p)| n * 10_i64.pow(p))
+        .sum::<i64>()
+}
+
 pub fn part2(xs: Vec<&str>) -> i64 {
-    // get list of number
-    // deduce the position
-    // map position to char
-    //
-    //let mapped: Vec<_> = mapped
-    //    .iter()
-    //    .map(|&(_, s2)| s2.trim().split(" ").collect::<Vec<&str>>())
-    //    .collect();
-    //println!("mapped[0..3]: {:?}", &mapped[0..3]);
-    //calculate char map form 10 signal
     let mapped: Vec<_> = xs.iter().map(|&line| line_mapper(line)).collect();
-    //println!("mapped[0..3]: {:?}", &mapped[0..3]);
     let signals: Vec<_> = mapped
         .iter()
         .map(|&(s1, s2)| {
@@ -70,151 +147,11 @@ pub fn part2(xs: Vec<&str>) -> i64 {
         })
         .clone()
         .collect();
-    //println!("mapped[0..3]: {:?}", &signals[0..3]);
 
-    let mut sum = 0;
-    for signal in signals {
-        let mut zero: HashSet<char> = HashSet::new();
-        let mut one: HashSet<char> = HashSet::new();
-        let mut two: HashSet<char> = HashSet::new();
-        let mut three: HashSet<char> = HashSet::new();
-        let mut four: HashSet<char> = HashSet::new();
-        let mut five: HashSet<char> = HashSet::new();
-        let mut six: HashSet<char> = HashSet::new();
-        let mut seven: HashSet<char> = HashSet::new();
-        let mut eight: HashSet<char> = HashSet::new();
-        let mut nine: HashSet<char> = HashSet::new();
-        let (ss, output) = signal;
-        for s in &ss {
-            let h: HashSet<char> = s
-                .clone()
-                .chars()
-                //.collect::<Vec<i64>>()
-                //.iter()
-                .collect();
-            if h.len() == 2 {
-                one = h.clone();
-                continue;
-            } else if h.len() == 3 {
-                seven = h.clone();
-                continue;
-            } else if h.len() == 4 {
-                four = h.clone();
-                continue;
-            } else if h.len() == 7 {
-                eight = h.clone();
-                continue;
-            }
-        }
-        for s in &ss {
-            let h: HashSet<char> = s
-                .clone()
-                .chars()
-                //.collect::<Vec<i64>>()
-                //.iter()
-                .collect();
-            let mut subcounter = 0;
-            for s2 in &ss {
-                let h2: HashSet<char> = s2
-                    .clone()
-                    .chars()
-                    //.collect::<Vec<i64>>()
-                    //.iter()
-                    .collect();
-                if h.len() == 5 && h != h2 && h.is_subset(&h2) {
-                    subcounter += 1;
-                }
-                if h.len() == 5 && one.is_subset(&h) {
-                    three = h.clone();
-                }
-            }
-            if subcounter == 3 {
-                five = h;
-            }
-        }
-        println!("one: {:?}", one);
-        println!("four: {:?}", four);
-        println!("three: {:?}", three);
-        println!("Five: {:?}", five);
-        println!("sev: {:?}", seven);
-
-        let mut charmap: Vec<char> = vec![' '; 7];
-        charmap[5] = five.intersection(&one).cloned().collect::<Vec<char>>()[0];
-        charmap[0] = seven.difference(&one).cloned().collect::<Vec<char>>()[0];
-        charmap[1] = four.difference(&three).cloned().collect::<Vec<char>>()[0];
-
-        //charmap[2] = one.difference(&five).cloned().collect::<Vec<char>>()[0];
-        charmap[2] = four.difference(&five).cloned().collect::<Vec<char>>()[0];
-        charmap[3] = four
-            .intersection(&three)
-            .cloned()
-            .collect::<HashSet<char>>()
-            .difference(&one)
-            .cloned()
-            .collect::<Vec<char>>()[0];
-
-        charmap[6] = five
-            .difference(&four)
-            .cloned()
-            .collect::<HashSet<char>>()
-            .difference(&(seven.difference(&one).cloned().collect::<HashSet<char>>()))
-            .cloned()
-            .collect::<Vec<char>>()[0];
-        charmap[4] = eight
-            .difference(&five)
-            .cloned()
-            .collect::<HashSet<char>>()
-            .difference(&one)
-            .cloned()
-            .collect::<Vec<char>>()[0];
-
-        dbg!(&charmap);
-
-        let s = charmap.iter().collect::<String>();
-        let order: &str = s.as_str().clone();
-        let mut d: HashMap<Vec<i64>, i64> = HashMap::new();
-        d.insert(vec![2, 5], 1);
-        d.insert(vec![1, 2, 3, 5], 4);
-        d.insert(vec![0, 2, 5], 7);
-        d.insert(vec![0, 1, 2, 3, 4, 5, 6], 8);
-
-        d.insert(vec![0, 2, 3, 4, 6], 2);
-        d.insert(vec![0, 2, 3, 5, 6], 3);
-        d.insert(vec![0, 1, 3, 5, 6], 5);
-
-        d.insert(vec![0, 1, 3, 4, 5, 6], 6);
-        d.insert(vec![0, 1, 2, 4, 5, 6], 0);
-        d.insert(vec![0, 1, 2, 3, 5, 6], 9);
-
-        fn char_to_num(charmap: &str, s: &str, map: &HashMap<Vec<i64>, i64>) -> i64 {
-            let p: HashSet<i64> = s
-                .chars()
-                .map(|c| charmap.find(c).unwrap() as i64)
-                //.collect::<Vec<i64>>()
-                //.iter()
-                .collect();
-            for (k, v) in map {
-                let khs: HashSet<i64> = k.iter().cloned().collect();
-                if khs == p {
-                    return *v;
-                }
-            }
-            dbg!(&charmap, s);
-
-            panic!("NOT REACH HERR");
-        }
-
-        dbg!(&output);
-
-        let r: Vec<i64> = output
-            .iter()
-            .map(|&s| char_to_num(&order, s, &d))
-            .zip((0..(output.len() as u32)).rev())
-            .map(|(n, p)| n * 10_i64.pow(p))
-            .collect();
-        sum += r.iter().sum::<i64>();
-    }
-    sum
+    signals
+        .iter()
+        .map(|signal| sum_a_line(signal.clone()))
+        .sum()
 }
 #[cfg(test)]
 mod tests {
