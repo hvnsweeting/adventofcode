be786453c7887b7dfe335e4d245ea5e80eea9610
Viet Hung Nguyen <hvn@familug.org>
2022-12-07 21:17:04 +0700

    p1

---
+++ 2022/src/clj2022/day07.clj
@@ -0,0 +1,104 @@
+(ns clj2022.day07
+  (:require [clojure.string :as str])
+  (:require [clojure.java.io :as io])
+  (:require [clojure.set :as set]))
+
+(defn build-tree [curdir
+                  state
+                  [cmd & rest]]
+  ;(println "build-tre" curdir state "CMD" cmd)
+  (if (empty? rest)
+    (let [[_ & out] (str/split-lines cmd)]
+      (merge state {curdir out}))
+
+    (if (str/starts-with? cmd "cd")
+      (let [[_, path] (str/split cmd #" ")]
+        (if (= path "/")
+          (recur "/" state rest)
+          (recur (str (.getCanonicalFile (io/file curdir path))) state rest)))
+
+                                        ; else
+      (let [[_ & out] (str/split-lines cmd)]
+        ;(println "OUT", out)
+        (recur curdir (merge state {curdir out}) rest)))))
+
+(defn file-sizes [files]
+  ;(println "files" files)
+  (->>
+   files
+
+   (map #(str/split % #" "))
+   (map first)
+   (map parse-long)
+   (reduce +)))
+
+(defn size [key tree]
+                                        ; path
+  (println "Calculating size of", key)
+  (let [v (tree key)]
+                                        ;(+ (map #(size % threshold tree) (tree key)))
+    (let [{dirs true files false} (group-by #(str/starts-with? % "dir ") (sort v)
+                                        ; filter dirs
+                                        ; turn to loop get size of them
+                                        ; plus 
+                                            )]
+      (println "files" files)
+      ;(println (file-sizes files))
+      ;
+      (+ (file-sizes files) (->> (map #(str/split % #" ") dirs)
+                                   (map second)
+                                   (map #(str (.getCanonicalFile (io/file key %))))
+                                   (map #(size % tree))
+                                   ;(println)
+                                   (reduce +)
+                                   ))
+      )))
+; way 2: 
+(defn day07-1 [input]
+                                        ;(println input)`
+  (println "START")
+  (def tree (->>
+
+             (str/split input #"\$")
+             (map #(str/trim %))
+             (filter #(not= "" %))
+             (build-tree "/" {})))
+
+  (->> (keys tree)
+       (map #(size % tree))
+       (filter #(< % 100000))
+       (reduce +)
+       ))
+
+(day07-1 "$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k")
+
+(day07-1 (slurp "src/clj2022/input07"))
+
+
+
+
+
+
+
