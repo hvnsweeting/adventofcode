60e55a3f369e43655a9c9b22cfe91f61ceba894d
Viet Hung Nguyen <hvn@familug.org>
2022-12-09 22:47:49 +0700

    Add day9

--- 2022/src/clj2022/day09.clj
+++ 2022/src/clj2022/day09.clj
@@ -3,13 +3,15 @@
   (:require [clojure.java.io :as io])
   (:require [clojure.set :as set]))
 
+;; since I wrote for part 1 which not directly usable for part2
+;; I use a simple python script to convert input to each move by 1 only
+;; python3 newinp9.py input09  
+
 (defn dbg [x]
   (prn "debug" x)
   x)
 
-
 (defn next-tail [[xhead yhead] [xtail ytail]]
-  ;(prn "calculate next-tail for" xhead yhead xtail ytail)
   (cond
     (and (>= 1 (abs (- xhead xtail))) (>= 1 (abs (- yhead ytail)))) (list xtail ytail)
 
@@ -19,26 +21,22 @@
                               (+ xtail 1)) ytail)
     :else (cond
             ; h top right of t
-            (and (= 1 (- xhead xtail)) (= 2 (- yhead ytail))) (list (+ 1 xtail) (+ 1 ytail))
-            (and (= 2 (- xhead xtail)) (= 1 (- yhead ytail))) (list (+ 1 xtail) (+ 1 ytail))
+            (and (< 0 (- xhead xtail)) (< 0 (- yhead ytail))) (list (+ 1 xtail) (+ 1 ytail))
+            (and (< 0 (- xhead xtail)) (< 0 (- yhead ytail))) (list (+ 1 xtail) (+ 1 ytail))
             ; h bot right of t
-            (and (= 1 (- xhead xtail)) (= -2 (- yhead ytail))) (list (+ 1 xtail) (- ytail 1))
-            (and (= 2 (- xhead xtail)) (= -1 (- yhead ytail))) (list (+ 1 xtail) (- ytail 1))
+            (and (< 0 (- xhead xtail)) (< (- yhead ytail) 0)) (list (+ 1 xtail) (- ytail 1))
+            (and (< 0 (- xhead xtail)) (< (- yhead ytail) 0)) (list (+ 1 xtail) (- ytail 1))
             ; h bot left of t
 
-            (and (= -1 (- xhead xtail)) (= -2 (- yhead ytail))) (list (- xtail 1) (- ytail 1))
-            (and (= -2 (- xhead xtail)) (= -1 (- yhead ytail))) (list (- xtail 1) (- ytail 1))
-            
+            (and (< (- xhead xtail) 0) (< (- yhead ytail) 0)) (list (- xtail 1) (- ytail 1))
+            (and (< (- xhead xtail) 0) (< (- yhead ytail) 0)) (list (- xtail 1) (- ytail 1))
+
             ; h top left of t
 
-            (and (= -1 (- xhead xtail)) (= 2 (- yhead ytail))) (list (- xtail 1) (+ ytail 1))
-            (and (= -2 (- xhead xtail)) (= 1 (- yhead ytail))) (list (- xtail 1) (+ ytail 1))
-            :else '(0 0)
-          
-           )
-
-    )
-  )
+            (and (< (- xhead xtail) 0) (> (- yhead ytail) 0)) (list (- xtail 1) (+ ytail 1))
+            (and (< (- xhead xtail) 0) (> (- yhead ytail) 0)) (list (- xtail 1) (+ ytail 1))
+            :else (do (prn "troubleshoot" xhead yhead xtail ytail)
+                      '(99 99)))))
 
 (assert (= '(1 1) (next-tail '(2 1) '(1 1))))
 (assert (= '(1 1) (next-tail '(1 1) '(1 1))))
@@ -56,38 +54,78 @@
 (assert (= '(-1 1) (next-tail '(-2 1) '(0 0))))
 (assert (= '(-1 1) (next-tail '(-1 2) '(0 0))))
 
-
 (defn move [[motion & rest] [xhead yhead] tail states]
 
-  (if (nil? motion) (merge states {tail 1})             ;TODO update states
+  (if (nil? motion) (merge states {tail 1})
+      (do
+        (def r (str/split motion #" "))
+        (def direction (first r))
+        (def n (second r))
+
+        (def newhead (cond
+                       (= direction "R") (list (+ xhead (parse-long n)) yhead)
+                       (= direction "L") (list (- xhead (parse-long n)) yhead)
+                       (= direction "U") (list xhead (+ yhead (parse-long n)))
+                       (= direction "D") (list xhead (- yhead (parse-long n)))))
+        (recur rest newhead (next-tail newhead tail) (merge states {tail 1})))))
+
+(defn day09-1 [input]
+
+  (def tail_states {})
+  (->>
+   (move (str/split-lines input) '(0 0) '(0 0) {})
+  ;(dbg)
+   (keys)
+   (count)
+   (println)))
+
+(day09-1 (slurp "src/clj2022/newinput09"))
+
+;;;; p2
+
+(defn updateall [[one & rest] head result]
+  ;(prn "updateall" one rest)
+  (if (nil? one) (reverse result)
+      (let [[tail state] one]
+        (let [newtail (next-tail head tail)]
+          (recur rest newtail (cons (list newtail (merge state {newtail 1})) result))))))
+
+(defn move9 [[motion & rest] [xhead yhead] states]
+
+  (if (nil? motion) states             ;TODO update states
       (do
         (def r (str/split motion #" "))
         (def direction (first r))
         (def n (second r))
         ;(prn rest xhead yhead tail states)
 
-        (cond
-          (= direction "R") (let [newhead (list (+ xhead (parse-long n)) yhead)] (recur rest newhead (next-tail newhead tail) (merge states {tail 1}) ))
-          (= direction "L") (let [newhead (list (- xhead (parse-long n)) yhead)] (recur rest newhead (next-tail newhead tail) (merge states {tail 1}) ))
-          (= direction "U") (let [newhead (list xhead (+ yhead (parse-long n)))] (recur rest newhead (next-tail newhead tail) (merge states {tail 1})))
-          (= direction "D") (let [newhead (list xhead (- yhead (parse-long n))) ] (recur rest newhead (next-tail newhead tail) (merge states {tail 1}) ))))))
-      
+        (def newhead (cond
+                       (= direction "R") (list (+ xhead (parse-long n)) yhead)
+                       (= direction "L") (list (- xhead (parse-long n)) yhead)
+                       (= direction "U") (list xhead (+ yhead (parse-long n)))
+                       (= direction "D") (list xhead (- yhead (parse-long n)))))
 
-      
+        ;(prn "HEAD AT" newhead)
+        (def newstates (updateall states newhead []))
+        (recur rest newhead newstates))))
 
-(defn day09-1 [input]
+(defn day09-2 [input]
 
   (def tail_states {})
   (->>
-  (move (str/split-lines input) '(0 0) '(0 0) {})
-  ;(dbg)
-  (keys)
-  (count)
-  (println)
-  )
-  
-  )
+   (move9 (str/split-lines input) '(0 0) [(list '(0 0) {})
+                                          (list '(0 0) {})
+                                          (list '(0 0) {})
+                                          (list '(0 0) {})
+                                          (list '(0 0) {})
+                                          (list '(0 0) {})
+                                          (list '(0 0) {})
+                                          (list '(0 0) {})
+                                          (list '(0 0) {})])
 
+   (last)
+   (second)
+   (keys)
+   (count)))
 
-(day09-1 (slurp "src/clj2022/newinput09"))
-
+(day09-2 (slurp "src/clj2022/newinput09"))
