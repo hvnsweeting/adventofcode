e36f141aa34db603180e139071e0268f29aff14a
Viet Hung Nguyen <hvn@familug.org>
2022-12-21 21:06:04 +0700

    Done 20 2

--- 2022/src/clj2022/day20.clj
+++ 2022/src/clj2022/day20.clj
@@ -15,9 +15,9 @@
 0
 4
 ")
-(defn parse-line [line]
+(defn parse-line [decryption-key line]
 
-  (parse-long line))
+  (* decryption-key (parse-long line)))
 
 (defn makemap [start]
   (loop [[k v & rest :as all] start
@@ -29,18 +29,27 @@
    (map #(into [] %) (partition 2 (interleave (vals m) (keys m))))
    (into {})))
 
-(defn visit [m from steps]
+(defn nth' [m from steps]
   (def s (rem steps (count m)))
   (def n (if (< s 0) (- (count m) (abs s) 1)
              s))
+  ;(prn "STEPS" steps "real n" n "m" m "from" from)
+  (nth (iterate m from) n))
+
+(defn visit [m from steps]
+  (def s (rem steps (dec (count m))))
+  (def n (if (< s 0) (- (count m) (abs s) 1)
+             s))
+  ;(prn "STEPS" steps "real n" n)
   (nth (iterate m from) n))
 
 (defn mix [[x & rest] m]
   (if (nil? x) m
       (do
+    ;(prn "mix" x (take (count m) (iterate m p0)))
         (def before (revmap m))
         (let [[_idx v] x]
-          (if (= 0 v) (recur rest m)
+          (if (= 0 (rem v (dec (count m)))) (recur rest m)
                                         ; x ---v----> y x
               (let [y (visit m x v)]
                                         ; beforex x afterx ... y after-y
@@ -54,22 +63,28 @@
 
                 (recur rest after)))))))
 
-(defn day20-1 [input]
+(defn day20-2 [input]
+  (def decryption-key 1)
+  (def decryption-key 811589153)
   (def links (->>
               (str/split-lines input)
-              (map parse-line)
+              (map #(parse-line decryption-key %))
               ;(dbg)
               (map-indexed vector)))
   (def m (makemap links))
 
   (do
     (def p0 [4094 0])
-    (def r (mix links m))
+    (def r (nth (iterate #(mix links %) m) 10))
+    ;(prn "RES" (map second (take (count links) (iterate r p0))))
+    ;(prn "map" r)
 
-    (->> (map second [(visit r p0 1000)
-                      (visit r p0 2000)
-                      (visit r p0 3000)])
+    (->> (map second [(nth' r  p0 1000)
+                      (nth' r p0 2000)
+                      (nth' r p0 3000)])
+         (dbg)
          (reduce +))))
 
-;(day20-1 sample)
-;(assert (= 8372 (day20-1 (slurp "src/clj2022/input20"))
+(day20-2 sample)
+
+(= 7865110481723 (day20-2 (slurp "src/clj2022/input20")))
