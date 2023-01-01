0f4a8f03cf6774090552c32edd922af9699cfc27
Viet Hung Nguyen <hvn@familug.org>
2023-01-01 21:46:42 +0700

    Done day 12

--- 2022/src/clj2022/day12.clj
+++ 2022/src/clj2022/day12.clj
@@ -9,46 +9,49 @@
 
 (defn to_map [y acc [r & rest]]
   ;(println r rest)
-   (if (nil? r) (merge acc)
+  (if (nil? r) (merge acc)
 
-       (do
-         (def newacc (->>
-                      (partition 2 (interleave (range (count r)) r))
-                      (map #(hash-map [ (first %) y] (second %)))
-                      (reduce merge)))
+      (do
+        (def newacc (->>
+                     (partition 2 (interleave (range (count r)) r))
+                     (map #(hash-map [(first %) y] (second %)))
+                     (reduce merge)))
 
-         (recur (+ y 1) (merge acc newacc) rest))))
+        (recur (+ y 1) (merge acc newacc) rest))))
 
-(defn good? [tree visited point that]
+(defn good? [tree point that]
   (and (contains? tree that)
-       (not (contains? visited that))
-       (or (= \E (tree that))
-           (<= (- (int (get tree that)) (int (get tree point))) 1))
-       ))
+       (<= (- (int (get tree that)) (int (get tree point))) 1)))
 
-(defn neighbors [tree visited [x, y]]
-  (let [ns [[(- x 1) y] [(+ x 1) y] [x (- y 1)] [x (+ y 1)] ]]
-    (prn ns)
-    (filter #(good? tree visited [x,y] %) ns)))
+(defn find-neighbors [tree [x, y]]
+  (let [ns [[(- x 1) y] [(+ x 1) y] [x (- y 1)] [x (+ y 1)]]]
+    (filter #(good? tree [x,y] %) ns)))
+
+(defn visited?
+  [v coll]
+  (contains? coll v))
 
 (defn bfs [start tree]
-  (loop [ret []
-         counter 0
-         visited #{}
-         queue (conj clojure.lang.PersistentList/EMPTY start)
-         ]
-    (if (seq queue)
-      (let [node (peek queue)]
-        (prn "VISITING" node (neighbors tree (conj visited node) node ))
-        
-        (if (= node \E)
-          (do (prn "WIN")
-              ret)
-          ;(recur (conj ret (inc counter)) 0 #{} (pop queue))
-          (recur ret 0 (conj visited node) (into (pop queue) (neighbors tree (conj visited node) node )))
-          ))
-    ret
-      )))
+  (loop [visited #{}
+         queue (conj clojure.lang.PersistentQueue/EMPTY start)
+         backpath {start 1}]
+    (if
+        ;(= v end)
+      ;(backpath v)
+     (empty? queue)
+      [visited backpath]
+      (let [v  (peek queue)
+            neighbors (find-neighbors tree v)
+            not-visited (filter (complement #(visited? % visited)) neighbors)
+            new-queue (apply conj (pop queue) not-visited)
+            new-backpath (merge backpath (into {} (map #(vector % (inc (backpath v))) not-visited)))
+            ;new-backpath (merge backpath (into {} (map #(vector % (inc (backpath v))) not-visited)))
+            ]
+        ;(prn "v" v "ns" neighbors "notvs" not-visited "q" new-queue)
+        ;(prn "point" v (count new-queue))
+        (if (visited? v visited)
+          (recur visited (pop queue) backpath)
+          (recur (conj visited v) new-queue new-backpath))))))
 
 (def sample "Sabqponm
 abcryxxl
@@ -56,19 +59,57 @@ accszExk
 acctuvwj
 abdefghi
 ")
+(defn get-path
+  [end start r m]
+  (if (= (m end) start)
+    r
+    (recur (m end) start (conj r end) m)))
+
+(defn shortest-path
+  [tree start end]
+
+  (def r (->> (bfs start tree)
+              (take-while #(not= end %))
+              (second)))
+
+  (if (contains? r end)
+    (dec (r end))
+    999999))
 
 (defn day12-1 [input]
   (def tree (->> (str/split-lines input)
-       (to_map 0 {} )
-    (dbg)
+                 (to_map 0 {})
+                 (dbg)
     ;(nth ins (- 1 %))
-    ))
-  (def tree2 (update tree [0 0] (fn [k] \a)))
+                 ))
+  (let [[start _] (first (filter (fn [[k v]] (= \S v)) tree))
+        [end _] (first (filter (fn [[k v]] (= \E v)) tree))]
 
-  (neighbors tree2 #{} [0 0])
-  (good? tree2 #{} [4 2] [5 2])
-  (bfs [0 0] tree2)
-  )
+                                        ; find start
+                                        ; find end
+                                        ;update high as A and Z
+    (def tree2 (assoc tree start \a))
+    (def tree2 (assoc tree2 end \z))
+    (prn "start " start "end" end)
 
-(day12-1 sample)
-;(assert (= 13860 (day12-1 (slurp "src/clj2022/input12"))))
+                                        ;(find-neighbors tree2 [0 0])
+
+    (shortest-path tree2 start end)))
+
+(defn day12-2 [input]
+  (def tree (->> (str/split-lines input)
+                 (to_map 0 {})
+                 (dbg)
+    ;(nth ins (- 1 %))
+                 ))
+  (let [[start _] (first (filter (fn [[k v]] (= \S v)) tree))
+        [end _] (first (filter (fn [[k v]] (= \E v)) tree))]
+
+    (def tree2 (assoc tree start \a))
+    (def tree2 (assoc tree2 end \z))
+    (prn "start " start "end" end)
+    (->>
+     (filter (fn [[k v]] (= v \a)) tree2)
+     (map (fn [[k v]] k))
+     (map #(shortest-path tree2 % end))
+     (apply min))))
